1988e3829efcc4e06f35aee6c2ec9ae805e02942
Antipow <antipow@gmail.com>
2018-03-29 09:57:35 +0300

    push to github

--- media.list/lang/ru/class.php
+++ media.list/lang/ru/class.php
@@ -1,1 +1,8 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$MESS ['PHONE'] = "Телефон";
+$MESS ['EMAIL'] = "Email";
+$MESS ['MESSAGE'] = "Сообщение";
+$MESS ['SUBJECT'] = "Тема";
+$MESS ['referer'] = "Страница перехода";
+$MESS ['konvers'] = "Отложенная конверсия";
