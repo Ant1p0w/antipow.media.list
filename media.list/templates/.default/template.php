23f8cf49ecce87619b9a3625858580d2ebf27719
Antipow <antipow@gmail.com>
2018-03-29 09:59:12 +0300

    remove empty files

--- media.list/templates/.default/template.php
+++ media.list/templates/.default/template.php
@@ -4,7 +4,7 @@ $this->setFrameMode(true);
 <div class="row">
     <? foreach ($arResult['ITEMS'] as $arFile) { ?>
         <div class="col-xl-3">
-            <div class="client-logo text-center p-2 mb-4">
+            <div class="text-center p-2 mb-4">
                 <img src="<?= $arFile['PATH'] ?>" class="img-fluid" alt="<?= $arFile['NAME'] ?>">
             </div>
         </div>
